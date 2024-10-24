dd7b8477456855898d84bd671cb3cf01481c308c
IsthatEmma Bautista <emmabautista.2008@gmail.com>
2024-10-24 17:21:47 +0000

    finished project

---
+++ wiki.py
@@ -0,0 +1,44 @@
+def binaryToDecimal():
+    
+    num = int(input("Enter a decimal number between 0 and 15 :) "))
+    
+  
+    if num == 0:
+        print("0000")
+    elif num == 1:
+        print("0001")
+    elif num == 2:
+        print("0010")
+    elif num == 3:
+        print("0011")
+    elif num == 4:
+        print("0100")
+    elif num == 5:
+        print("0101")
+    elif num == 6:
+        print("0110")
+    elif num == 7:
+        print("0111")
+    elif num == 8:
+        print("1000")
+    elif num == 9:
+        print("1001")
+    elif num == 10:
+        print("1010")
+    elif num == 11:
+        print("1011")
+    elif num == 12:
+        print("1100")
+    elif num == 13:
+        print("1101")
+    elif num == 14:
+        print("1110")
+    elif num == 15:
+        print("1111")
+    else:
+        print("Please enter a number between 0 and 15!:)")
+
+
+binaryToDecimal()
+
+
